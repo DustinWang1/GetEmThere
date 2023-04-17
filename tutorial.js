64dbba3aa4395da7d3a9eb12c8c93b9c1c97fbdd
DustinWang1 <Wangd385@gmail.com>
2023-04-16 18:56:29 -0700

    Added path and switches as separate objects.

--- tutorial.js
+++ tutorial.js
@@ -12,7 +12,9 @@ export default class Tutorial extends Phaser.Scene {
     this.load.image('god', 'Assets/God eye.png');
     this.load.image('pupil', 'Assets/God pupil.png')
     this.load.image('popup', 'Assets/PopupBG.png');
-
+    this.load.image('tiles', 'Assets/PathTiles.png');
+    this.load.spritesheet('tilesheet', 'Assets/PathTiles.png',{frameWidth: 32, frameHeight: 32});
+    this.load.tilemapTiledJSON('map', 'Assets/TutorialMap.tmj');
   }
 
   create() {
@@ -48,8 +50,22 @@ export default class Tutorial extends Phaser.Scene {
   }
 
   createPath() {
+    this.map = this.make.tilemap({key: 'map'});
+    this.tileset = this.map.addTilesetImage('PathTiles', 'tiles');
+    this.staticPaths = this.map.createLayer('Static', this.tileset, 0, 32);
+    this.switches = this.add.group();
+    this.map.getObjectLayer('Switch').objects.forEach((tempSwitch) => {
+      if(this.tiledObjectIsSwitch(tempSwitch)) {
+        this.switches.add(new Switch(tempSwitch, this)); 
+      }
+    });
+    
   }
   
+  tiledObjectIsSwitch(object) {
+    var TypeProperty = object.properties.findIndex((property) => {return property.name === "Type"});
+    return object.properties[TypeProperty].value === "Switch";
+  }
 }
 
 class God {
@@ -87,5 +103,23 @@ class God {
 
   calculateDeltaPoint(angle, radius) {
     return new Phaser.Math.Vector2(radius*Math.cos(angle), radius*Math.sin(angle));
+  }
+}
+
+class Switch extends Phaser.GameObjects.Sprite {
+  constructor(TiledObject, scene) {
+    super(scene, TiledObject.x + (16), TiledObject.y + (16), 'tilesheet', 1);
+    this.object = TiledObject;
+    this.setInitialOrientation(TiledObject);
+    scene.add.existing(this);
+  }
+
+  setInitialOrientation() {
+    this.setFrame(this.getInitialFrameFromTiledObject());
+  }
+
+  getInitialFrameFromTiledObject() {
+    var index = this.object.properties.findIndex((property) => {return property.name === "initialFrame"});
+    return this.object.properties[index].value;
   }
 }
